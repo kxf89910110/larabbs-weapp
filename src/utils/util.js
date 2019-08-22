52674caf386641d9fd592b5ebc54c88625601915
kxf89910110 <1052831447@qq.com>
2019-08-22 02:41:48 +0000

    add moment.js

---
+++ src/utils/util.js
@@ -0,0 +1,11 @@
+import moment from 'moment'
+// import 'moment/locale/en'
+
+const diffForHumans = (date, format='YYYYMMDD H:mm:ss') => {
+  moment.locale('en')
+  return moment(date, format).fromNow()
+}
+
+export default {
+  diffForHumans
+}
