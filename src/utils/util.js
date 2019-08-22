b797d968176117f6ad85d3bda7070858a2d40944
kxf89910110 <1052831447@qq.com>
2019-08-22 03:23:46 +0000

    index add categories

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 // import 'moment/locale/en'
 
-const diffForHumans = (date, format='YYYYMMDD H:mm:ss') => {
+const diffForHumans = (date, format = 'YYYYMMDD H:mm:ss') => {
   moment.locale('en')
   return moment(date, format).fromNow()
 }
