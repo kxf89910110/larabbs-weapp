d01386e49c6022daadf3da1d1411427a5e1b775c
kxf89910110 <1052831447@qq.com>
2019-08-23 06:52:28 +0000

    user perms

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -34,7 +34,7 @@ export default class ReplyMixin extends wepy.mixin {
         replies.forEach((reply) => {
           // 控制是否可以删除
           reply.can_delete = this.canDelete(user, reply)
-          console.log('reply.can_delete',reply,reply.can_delete)
+          console.log('reply.can_delete', reply, reply.can_delete)
           reply.created_at_diff = util.diffForHumans(reply.created_at)
         })
         // 如果reset不为true则合并 this.replies；否则直接覆盖
@@ -63,7 +63,8 @@ export default class ReplyMixin extends wepy.mixin {
       return false
     }
 
-    return (reply.user_id === user.id)
+    // 用户为回复发布者 或 有管理内容权限
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
   async onPullDownRefresh() {
     this.noMoreData = false
@@ -114,7 +115,7 @@ export default class ReplyMixin extends wepy.mixin {
             duration: 2000
           })
           // 将删除了的回复移除
-          this.replies = this.replies .filter((reply) => reply.id !== replyId)
+          this.replies = this.replies.filter((reply) => reply.id !== replyId)
           this.$apply()
         }
 
